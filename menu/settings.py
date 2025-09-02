028753d314f66c33f8a0a6d73df8b36f9494b8c9
Sardor Mirniyozov <sardormirniyozov63@gmail.com>
2025-09-02 12:02:03 +0500

    Jgatim

--- menu/settings.py
+++ menu/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-+z2o$_&shk(n_#-4p)0(pd3w-x--vmcd9gl)wb2tf8z^_-k2-f"
+SECRET_KEY = 'django-insecure-+z2o$_&shk(n_#-4p)0(pd3w-x--vmcd9gl)wb2tf8z^_-k2-f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
